24af51de607c75266809781cea463a2e5826d624
Kocksxdoze <famelezzz@gmail.com>
2025-10-22 15:28:00 +0500

    save

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,7 @@
 export function getApiBaseUrl() {
   if (typeof window !== "undefined") {
     const host = window.location.hostname;
+    console.log(host);
     return `http://${host}:4000`;
   }
 
